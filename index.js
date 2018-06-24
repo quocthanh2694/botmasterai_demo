0c2adfbbb0edf93ed3e410a7427f342ce078cc49
DESKTOP-VR4QC0T\ProBook <quocthanh2694@gmail.com>
2018-06-24 12:38:08 +0700

    make it better

--- index.js
+++ index.js
@@ -13,14 +13,21 @@
 //     console.log("Server listening at: " + listener.address().address + ":" + listener.address().port);
 
 
-    
+
 // });
 
+const Botmaster = require('botmaster');
 
+const express = require('express');
+const app = express();
+const myServer = app.listen(process.env.PORT || 3000, process.env.address);
+const botmaster = new Botmaster({ server: myServer });
+myServer.on('listening', () => {
+    console.log('My express app is listening and its server is used in Botmaster');
+})
 
 // bot master
-const Botmaster = require('botmaster');
-const botmaster = new Botmaster();
+
 const MessengerBot = require('botmaster-messenger');
 const messengerSettings = {
     credentials: {
@@ -29,7 +36,7 @@ const messengerSettings = {
         fbAppSecret: process.env.fbAppSecret,
     },
     // domain: '127.0.0.1',
-    port: process.env.PORT || 3000,
+    // port: process.env.PORT || 3000,
     webhookEndpoint: 'webhook', // botmaster will mount this webhook on https://Your_Domain_Name/messenger/webhook1234
 };
 console.log(messengerSettings);
@@ -41,11 +48,11 @@ botmaster.use({
     type: 'incoming',
     name: 'my-incoming-middleware',
     controller: (bot, update) => {
-      console.log(update);
+        console.log(update);
 
-      return bot.reply(update, 'Hi, You say: ' + update.message.text );
+        return bot.reply(update, 'Hi, You say: ' + update.message.text);
     }
-  });
- console.log(botmaster);
+});
+console.log(botmaster);
 
 
