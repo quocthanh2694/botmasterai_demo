afa751d956467b98d4e0b90b6e768741344e88a2
DESKTOP-VR4QC0T\ProBook <quocthanh2694@gmail.com>
2018-06-24 12:04:55 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// const express = require('express');
+// const app = express();
+
+// bot master
+const Botmaster = require('botmaster');
+const botmaster = new Botmaster();
+const MessengerBot = require('botmaster-messenger');
+const messengerSettings = {
+    credentials: {
+        verifyToken: process.env.verifyToken,
+        pageToken: process.env.pageToken,
+        fbAppSecret: process.env.fbAppSecret,
+    },
+    // domain: '127.0.0.1',
+    webhookEndpoint: 'webhook', // botmaster will mount this webhook on https://Your_Domain_Name/messenger/webhook1234
+};
+
+const messengerBot = new MessengerBot(messengerSettings);
+
+botmaster.addBot(messengerBot);
+
+botmaster.use({
+    type: 'incoming',
+    name: 'my-incoming-middleware',
+    controller: (bot, update) => {
+      console.log(update);
+      return bot.reply(update, 'Hi, You say: ' + update.message.text );
+    }
+  });
+ 
+// // ///// express
+// app.get('/', (req, res) => {
+//     // const messengerBot = new MessengerBot(messengerSettings);
+//     // console.log(messengerBot);
+//     res.send('Hello World!')
+// });
+// var listener = app.listen(process.env.PORT || 3000, process.env.address, () => {
+//     console.log("Server listening at: " + listener.address().address + ":" + listener.address().port)
+// });
